e6d4422b7230258a352901bb55537172c94c49f9
fabien0102 <fabien0102@planee.fr>
2018-04-10 10:26:18 +0200

    Deal with OPTIONS preflight call

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,3 +1,17 @@
 import { server } from './_server'
 
-export const handler = server
+export const handler = (event, context, cb) => {
+  // Deal with 'OPTIONS' prefight call
+  if (event.httpMethod !== 'POST' || !event.body) {
+    cb(null, {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
+      body: ''
+    })
+  } else {
+    server(event, context, cb)
+  }
+}
